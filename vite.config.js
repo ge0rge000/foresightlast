d6234b832033c0149b084946fbcbb59633668422
ge0rge000 <gogosony124@gmail.com>
2024-08-09 14:08:00 +0300

    asdsa

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,8 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // specify the output directory
+        manifest: true,          // enable manifest generation
+    },
 });
